df3e78afd2a7226db599f7d79a8bd23b95cf3d3d
alessandrayoon <alessandra.yoon@gmail.com>
2016-09-06 11:23:24 -0700

    Switched from class to java

---
+++ APLesson_01/secondProg.java
@@ -0,0 +1,19 @@
+public class secondProg
+{
+	public static void main(String[]args)
+	{
+		System.out.println("  \\\\ ");
+	    System.out.println("   \\\\_ ");
+	    System.out.println("   ( _\\ ");
+	    System.out.println("   / \\__ ");
+	    System.out.println("  / _/`\"`");
+	    System.out.println(" {\\  )_");
+	    System.out.println("  `\"\"\"`");
+	}
+}
+	
+	
+	
+	
+	
+	
