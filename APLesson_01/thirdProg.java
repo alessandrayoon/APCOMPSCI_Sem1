61b849f867ab5c8b46e78ec3c318a60f3b8f3906
alessandrayoon <alessandra.yoon@gmail.com>
2016-09-06 11:20:32 -0700

    Added Files

---
+++ APLesson_01/thirdProg.java
@@ -0,0 +1,10 @@
+public class thirdProg
+{
+	public static void main(String[]args)
+	{
+		System.out.println(" __");
+		System.out.println("( o>");
+		System.out.println("///\\");
+		System.out.println("\\V_/_");
+	}
+	}
