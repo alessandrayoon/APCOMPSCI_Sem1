bdce6f242910146fc2d0814e53be61372d659bb7
alessandrayoon <alessandra.yoon@gmail.com>
2016-09-08 11:25:54 -0700

    Added Lesson 02

---
+++ APLesson_02/Lab_02.java
@@ -0,0 +1,29 @@
+public class Lab_02
+{
+	public static void main(String[]args)
+	{
+		int x = 2;
+		int y = 4;
+		int multiplication = x * y;
+		
+		System.out.println( x + " multiplied by " + y + " is " + multiplication);
+		
+		String a = "Alessandra Yoon";
+		String b = "4617 Corte Sol Del Dios";
+		String c = "San Diego";
+		String d = "92130";
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c + "," + d);
+		
+		int l = 1;
+		int w = 2;
+		int h = 3;
+		int Surfacearea = 2 * w * l + 2 * l * h + 2 * h * w;
+		
+		
+		System.out.println("The surface area of your rectangle is" + " " + Surfacearea);
+	}
+}	
+		
