3afa04e280f14c696e363a2b8523b8ddb198ccaa
alessandrayoon <YoonA6253@sduhsd.lan>
2016-10-14 11:16:05 -0700

    saved

---
+++ AP Lesson_05/ReturnCircle.java
@@ -0,0 +1,29 @@
+import java.util.Scanner; //import Statement
+public class ReturnCircle
+{
+	
+	
+	public static void main(String[]args)
+	{
+		Circle area = new Circle();
+		
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println("Please enter the length of the radius:");
+		double r = keyboard.nextDouble();
+		
+		print ("The area of a circle with a radius of " + r +  " is " , calcArea(r));
+		
+		
+	
+	}
+	public static double                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             calcArea(double r)
+	
+	{
+		 return 3.14*r*r;
+	}
+	public static void print(String text, double area)
+	{
+		System.out.print(text + area);
+	}
+}
