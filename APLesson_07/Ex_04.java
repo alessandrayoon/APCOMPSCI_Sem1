bb16ed880c2c2f679039427b2e9fa630cdffbcb1
alessandrayoon <YoonA6253@sduhsd.lan>
2016-11-07 11:56:59 -0800

    Finishes lesson and labs in 7

---
+++ APLesson_07/Ex_04.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Ex_04
+{
+	static String sentence;
+	
+	
+	public static void main(String[]args)
+	{
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Please enter a sentence: ");
+		sentence = kb.nextLine();
+		
+		
+			while(sentence.indexOf("a") >= 0)
+			{
+				sentence = sentence.substring(0, sentence.indexOf("a")) + "@" + sentence.substring(sentence.indexOf("a") +1);
+			}
+		
+		
+		System.out.println("Your new sentence is... " + sentence);
+		
+	}
+	
+}
