4f0e7d864af99ddfe79f795926d9e9041e6a0382
alessandrayoon <YoonA6253@sduhsd.lan>
2016-11-29 12:09:07 -0800

    Finished Lab 09

---
+++ APLesson_09/NewLetter.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class NewLetter
+{
+	public static void main(String[]args)
+	{
+		Scanner kb = new Scanner(System.in);
+		String[] words = new String[5];
+		
+		System.out.println("Please enter five words ");
+		
+		for(int i = 0; i < words.length; i++)
+		{
+			words[i] = kb.next();
+		}
+		
+			for (String word : words)
+			{
+				System.out.print(" ");
+			}
+			first(words);
+	}
+	
+	public static void first(String[] words)
+	{
+		for(int i = 0; i<=4; i++)
+		{
+			System.out.print(words[i].substring(0,1)  + " ");
+		}
+	}
+}
